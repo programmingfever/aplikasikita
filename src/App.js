b56b8da030a20601256c1989ac8cbd59d7a44f07
programmingfever <rozak.programming@gmail.com>
2020-09-28 23:08:08 +0700

    latihan react

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function HelloWorld(){
+  return <div style={{ backgroundColor: 'yellow'}}>Abdul Rozak</div>;
+}
+function Tombol(){
+  return <button style={{ backgroundColor: 'lightcoral'}} onClick={() => alert('Selamat Datang')}>Klik</button>
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <HelloWorld />
+        <Tombol/>
         <a
           className="App-link"
           href="https://reactjs.org"
